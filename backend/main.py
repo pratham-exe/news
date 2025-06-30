bd555cf7fd3b0da3f73146ab7fab152e9a938cc8
pratham-exe <prathamrao001@gmail.com>
2025-06-30 20:51:45 +0530

    category api call

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,24 @@
+import os
+
+import requests as rq
+from dotenv import load_dotenv
 from fastapi import FastAPI
 
+load_dotenv()
 app = FastAPI()
 
+news_api_key = os.getenv("NEWS_API_KEY")
 
-@app.post("/news")
-def home():
-    return {"Hello News"}
+
+@app.post("/get-category-news/{requested_category}")
+def get_category_top_headlines(requested_category):
+    output = []
+    url = f"https://newsapi.org/v2/top-headlines?category={requested_category}&apiKey={news_api_key}"
+    category_top_headlines = rq.get(url).json()
+    articles_from_category = category_top_headlines.get("articles")
+    for each in articles_from_category:
+        if each.get("description") is not None:
+            output.append(each.get("description"))
+        else:
+            output.append(each.get("title"))
+    return output
--- frontend/app.py
+++ frontend/app.py
@@ -1,6 +1,38 @@
+import json
+
 import requests as rq
 import streamlit as st
 
-st.title("News")
-res = rq.post("http://localhost:8000/news")
-st.write(res.content)
+st.title("News Reporter Agent")
+
+categories, user_query = st.tabs(["Categories", "Others"])
+
+with categories:
+    category = st.selectbox(
+        "Select category",
+        [
+            "None",
+            "Business",
+            "Entertainment",
+            "General",
+            "Health",
+            "Science",
+            "Sports",
+            "Technology",
+        ],
+    )
+    if category != "None":
+        category_search_button = st.button("Get news")
+        if category_search_button:
+            category_news = rq.post(
+                f"http://localhost:8000/get-category-news/{category}"
+            )
+            category_top_headlines = json.loads(category_news.content.decode("utf-8"))
+            for each in category_top_headlines:
+                st.markdown(each)
+
+with user_query:
+    user_query_news = st.text_input("Ask me anything")
+    if user_query_news:
+        st.write("Query: ", user_query_news)
+        user_query_search_button = st.button("Get news")
