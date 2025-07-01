7404b099fbb565dcda156745d5b08d05edc6e5da
pratham-exe <prathamrao001@gmail.com>
2025-07-01 15:04:01 +0530

    query api call

--- backend/main.py
+++ backend/main.py
@@ -22,3 +22,17 @@ def get_category_top_headlines(requested_category):
         else:
             output.append(each.get("title"))
     return output
+
+
+@app.post("/get-query-news/{user_query}")
+def get_user_query_top_headlines(user_query):
+    output = []
+    url = f"https://newsapi.org/v2/top-headlines?q={user_query}&apiKey={news_api_key}"
+    query_top_headlines = rq.get(url).json()
+    articles_from_query = query_top_headlines.get("articles")
+    for each in articles_from_query:
+        if each.get("description") is not None:
+            output.append(each.get("description"))
+        else:
+            output.append(each.get("title"))
+    return output
--- frontend/app.py
+++ frontend/app.py
@@ -29,10 +29,17 @@ with categories:
             )
             category_top_headlines = json.loads(category_news.content.decode("utf-8"))
             for each in category_top_headlines:
-                st.markdown(each)
+                st.markdown(each, unsafe_allow_html=True)
 
 with user_query:
     user_query_news = st.text_input("Ask me anything")
     if user_query_news:
         st.write("Query: ", user_query_news)
-        user_query_search_button = st.button("Get news")
+        user_query_search_button = st.button("Get query news")
+        if user_query_search_button:
+            query_news = rq.post(
+                f"http://localhost:8000/get-query-news/{user_query_news}"
+            )
+            query_top_headlines = json.loads(query_news.content.decode("utf-8"))
+            for each in query_top_headlines:
+                st.markdown(each, unsafe_allow_html=True)
